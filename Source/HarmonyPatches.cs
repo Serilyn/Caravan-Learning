6ca52c1ba0ade522266922153de9f882931ecfce
Serilyn <serilynx@gmail.com>
2022-11-29 11:29:58 -0800

    Initial Commit

---
+++ Source/HarmonyPatches.cs
@@ -0,0 +1,28 @@
+﻿using HarmonyLib;
+using Verse;
+using RimWorld;
+
+namespace Caravan_Learning
+{
+    public class HarmonyPatches : Mod
+    {
+        public HarmonyPatches(ModContentPack content) : base(content)
+        {
+            var harmony = new Harmony("caravanLearning");
+            harmony.PatchAll();
+        }
+
+        [HarmonyPatch(typeof(Need_Learning),"IsFrozen", MethodType.Getter)]
+        class Patch_IsFrozen
+        {
+            static void Postfix(ref bool __result, Pawn ___pawn)
+            {
+                if (___pawn.Map == null)
+                    __result = true;
+            }
+        }
+
+    }
+
+
+}
