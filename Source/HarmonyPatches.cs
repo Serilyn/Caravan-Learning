75be7c68d54875e6183589f784c1f373e6f3b4cf
Serilyn <serilynx@gmail.com>
2022-12-01 12:41:41 -0800

    Growth tier now increases on caravan.

--- Source/HarmonyPatches.cs
+++ Source/HarmonyPatches.cs
@@ -1,6 +1,7 @@
 ﻿using HarmonyLib;
 using Verse;
 using RimWorld;
+using System.Reflection;
 
 namespace Caravan_Learning
 {
@@ -12,6 +13,7 @@ namespace Caravan_Learning
             harmony.PatchAll();
         }
 
+        //Freeze learning decay
         [HarmonyPatch(typeof(Need_Learning),"IsFrozen", MethodType.Getter)]
         class Patch_IsFrozen
         {
@@ -19,6 +21,23 @@ namespace Caravan_Learning
             {
                 if (___pawn.Map == null)
                     __result = true;
+            }
+        }
+
+        //Allow our growth tier to increase on caravan
+        [HarmonyPatch(typeof(Pawn_AgeTracker), "GrowthPointsPerDay", MethodType.Getter)]
+        class Patch_NeedInterval
+        {
+            static void Postfix(ref float __result, Pawn_AgeTracker __instance, Pawn ___pawn)
+            {
+                Need_Learning learning = ___pawn.needs.learning;
+                // It's okay if learning is frozen while on caravan so skip the suspended check.
+                if (learning != null && ___pawn.Map == null)
+                {
+                    MethodInfo methodInfo = typeof(Pawn_AgeTracker).GetMethod("GrowthPointsPerDayAtLearningLevel", BindingFlags.NonPublic | BindingFlags.Instance);
+                    var parameters = new object[] { learning.CurLevel };
+                    __result = (float)methodInfo.Invoke(__instance, parameters);
+                }
             }
         }
 
